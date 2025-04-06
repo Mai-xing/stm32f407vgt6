333a9de6da38ddf9f75d3cc6e7d876c7fd745824
Mai-xing <lsy6515@126.com>
2025-04-06 18:29:54 +0800

    2

--- zigbee_demo/MDK-ARM/startup_stm32f407xx.s
+++ zigbee_demo/MDK-ARM/startup_stm32f407xx.s
@@ -16,10 +16,13 @@
 ;* <h2><center>&copy; Copyright (c) 2017 STMicroelectronics.
 ;* All rights reserved.</center></h2>
 ;*
-;* This software component is licensed by ST under BSD 3-Clause license,
+;* This software component is licensed by ST under BSD 3-Clause license，
 ;* the "License"; You may not use this file except in compliance with the
 ;* License. You may obtain a copy of the License at:
 ;*                        opensource.org/licenses/BSD-3-Clause
+
+
+
 ;*
 ;*******************************************************************************
 ;* <<< Use Configuration Wizard in Context Menu >>>
